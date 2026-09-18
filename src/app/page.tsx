7a68e621ebead437685f3b74a3b18a219799b2e9
Safiullah <safiarain273@gmail.com>
2026-09-18 19:38:12 +0500

    section commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
       {/* <StickyHeader /> */}
       <HeroSection />
       <TrustSignalsSection />
-      <AIProducts />
+      {/* <AIProducts /> */}
       {/* <LeadMagnet /> */}
       <EnterprisePainPointsSection />
       <WhyBawdicSoftSection />
